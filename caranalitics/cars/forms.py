6ba3d80b4cfd4f36acec4b7398fab187bcdb47cf
Kirill <kirill361216@gmail.com>
2026-08-03 11:29:49 +0200

    Crud is created, basic layout is made

--- caranalitics/cars/forms.py
+++ caranalitics/cars/forms.py
@@ -2,5 +2,6 @@ from django import forms
 from .models import Car
 
 class CarForm(forms.ModelForm):
-    model = Car
-    fields = "__all__"
+    class Meta:
+        model = Car
+        fields = "__all__"
